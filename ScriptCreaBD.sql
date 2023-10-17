8382373e5468f302e5491246b0bdf724ddd77918
KubDroid <ale.cubillo@gmail.com>
2023-10-17 13:53:42 -0600

    first commit

---
+++ ScriptCreaBD.sql
@@ -0,0 +1,9 @@
+#La siguiente línea de código sirve para crear nuestra base de datos en el servidor
+
+create database prueba;
+
+#Esta sera nuestra segunda Base da datos
+
+create database prueba2;
+
+show databases;
---
+++ ScriptEliminaBD.sql
@@ -0,0 +1,9 @@
+#La siguiente línea de código sirve para crear nuestra base de datos en el servidor
+
+drop database prueba;
+
+#Esta sera nuestra segunda Base da datos
+
+drop database prueba2;
+
+show databases;
